5691d38a69e32cbff422cd63328f84a9da215f28
bobbyk68 <bobbyk68@gmail,com>
2025-08-05 18:59:47 +0100

    rule

--- src/main/java/com/example/dslgen/RuleGeneratorApp.java
+++ src/main/java/com/example/dslgen/RuleGeneratorApp.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+
 public class RuleGeneratorApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
